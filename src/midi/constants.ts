1a9b247dad5f3305741733f41413ecc2d5ca6267
l3n-d3v <l3n.r3idy@gmail.com>
2023-04-05 20:28:58 +0100

    Add constants

--- src/midi/constants.ts
+++ src/midi/constants.ts
@@ -3,3 +3,16 @@ export enum ServiceMessages {
 	Disabled = '::WebMidi disabled::',
 }
 
+export const Constants = {
+	WebMidi: {
+		Disabled: {
+			Error: 'Failed to disable WebMidi',
+			Success: 'WebMidi disabled',
+		},
+		Enabled: {
+			Error: 'Failed to enable WebMidi',
+			Success: 'WebMidi enabled',
+		},
+	
+	}
+}
