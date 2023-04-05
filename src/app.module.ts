fe01676e734e64f0d3a0882cd6656ab350b576f2
l3n-d3v <l3n.r3idy@gmail.com>
2023-04-05 20:28:22 +0100

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MidiModule } from './midi/midi.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MidiModule,
   ],
   controllers: [AppController],
