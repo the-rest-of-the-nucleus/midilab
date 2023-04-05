bf8ce2fe4f98cc57141e1d946ab0e2aa1e20ee81
l3n-d3v <l3n.r3idy@gmail.com>
2023-04-05 20:31:01 +0100

    Add logic to enable and disable WebMidi at the module level

--- src/midi/midi.module.ts
+++ src/midi/midi.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { MidiService } from './midi.service';
 import { MidiController } from './midi.controller';
+import { WebMidi } from 'webmidi';
+import { Constants } from './constants';
+
 
 @Module({
   controllers: [MidiController],
   providers: [MidiService]
 })
-export class MidiModule {}
+export class MidiModule implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(MidiModule.name);
+
+  onModuleInit() {
+    WebMidi.enable()
+    .then(() => this.logger.log(Constants.WebMidi.Enabled.Success))
+    .catch(() => this.logger.error(Constants.WebMidi.Enabled.Error));
+  }
+  
+  onModuleDestroy() {
+    WebMidi.disable()
+    .then(() => this.logger.log(Constants.WebMidi.Disabled.Success))
+    .catch(() => this.logger.error(Constants.WebMidi.Disabled.Error));  
+  }
+}
